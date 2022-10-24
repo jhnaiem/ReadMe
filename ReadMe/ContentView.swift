2c82544cd613d57ab0636e0ab688415b8e076a26
Naiem <jahid.hasan@monstar-lab.com>
2022-10-24 11:11:35 +0600

    Initial Commit

---
+++ ReadMe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by BD Macbook Air B on 24/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by BD Macbook Air B on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
